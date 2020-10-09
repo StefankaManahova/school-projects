c28889527d099f892686f6cb90d9eb709a119d2b
StefankaManahova <71600598+StefankaManahova@users.noreply.github.com>
2020-10-09 15:33:36 +0300

    Add files via upload

--- AnswerKeys.java
+++ AnswerKeys.java
@@ -1,10 +1,21 @@
 package edu.smg;
 
 import java.util.Scanner;
+
 public class AnswerKeys {
 
 	public static void main(String[] args) {
-		
+	Scanner sc = new Scanner (System.in);
+	final int numberOfQuestions = 10;
+	final int numberOfStudents = 8;
+	
+	String[] key = new String[numberOfQuestions];
+    System.out.println("What is the answer key?");
+    for(int i=0; i<numberOfQuestions; i++)
+    {
+    	key[i] = sc.nextLine();
+    	
+    }
 
 	}
 
