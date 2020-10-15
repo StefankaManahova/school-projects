a055eeffe14331bdcbeef54b3c593f7841b36f33
User <User@DESKTOP-LO6F70E>
2020-10-15 21:43:35 +0300

    Answer keys programme

--- Hello11E/src/edu/smg/AnswerKeys.java
+++ Hello11E/src/edu/smg/AnswerKeys.java
@@ -6,8 +6,8 @@ public class AnswerKeys {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner (System.in);
-	final int numberOfQuestions = 10;
-	final int numberOfStudents = 8;
+	final int numberOfQuestions = 6;
+	final int numberOfStudents = 3;
 	
 	String[] key = new String[numberOfQuestions];
     System.out.println("What is the answer key?");
@@ -15,6 +15,25 @@ public class AnswerKeys {
     {
     	key[i] = sc.nextLine();
     	
+    }
+  String[][] answers = new String[numberOfStudents][numberOfQuestions];
+    
+    for(int j=0; j<numberOfStudents; j++) {
+    	System.out.println("What are the answers of the "+(j+1)+". student?");
+    	for(int i=0; i<numberOfQuestions; i++) {
+    		answers[j][i] = sc.nextLine();
+    	}
+    }
+    int[] score = new int[numberOfStudents];
+    
+    for(int j=0; j<numberOfStudents; j++) {
+    	for(int i=0; i<numberOfQuestions; i++) {
+    		score[j] += (answers[j][i].equals(key[i]))? 1 : 0;
+    	}
+    }
+    
+    for( int j=0; j<numberOfStudents; j++) {
+    	System.out.println("The "+(j+1)+". student's score is "+ score[j]+ ((score[j]!=1)?" points." : " point."));
     }
 
 	}
